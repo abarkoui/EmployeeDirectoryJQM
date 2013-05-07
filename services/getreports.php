2fa87985e800252c20b6229b254a4ef7f21bd43f
Barkooui <anis.barkoui@mail.com>
2013-05-07 11:25:14 +0200

    id problem fixed

--- services/getreports.php
+++ services/getreports.php
@@ -9,7 +9,7 @@ try {
 	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$stmt = $dbh->prepare($sql);  
-	$stmt->bindParam("id", $_GET[id]);
+	$stmt->bindParam("id", $_GET["id"]);
 	$stmt->execute();
 	$employees = $stmt->fetchAll(PDO::FETCH_OBJ);
 	$dbh = null;
